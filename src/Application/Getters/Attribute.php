fab3c6b1208460300f1d1e6c51d8970d79223eb5
jason-napolitano <jnapolitanoit@gmail.com>
2026-07-07 07:25:12 -0700

    Added CreatesBinding Added the CreatesBinding attribute to service providers, which allows a provider to pass the `id` and `concrete` class that the provider will bind to the container.

--- src/Application/Getters/Attribute.php
+++ src/Application/Getters/Attribute.php
@@ -4,38 +4,57 @@ namespace FrameworkFactory\Application\Getters {
 
     class Attribute
     {
-        /** @var array $cache the cached array of attributes */
+        /** @var array $reflections cached reflection classes */
+        private static array $reflections = [];
+
+        /** @var array cached attributes */
         private static array $cache = [];
 
         /**
          * Returns a cached instance of an attribute passed in at
-         * class level
+         * class level.
          *
          * @param string $class
          * @param string $attribute
          *
-         * @return object
-         *
-         * @throws \ReflectionException
+         * @return object|null
          */
-        public static function get(string $class, string $attribute): object
+        public static function get(string $class, string $attribute): ?object
         {
-            // designate the cache key
             $key = "{$class}:{$attribute}";
 
-            // if the cache value is not already set
-            if (!isset(self::$cache[$key])) {
-                // create a new reflection instance
-                $reflection = new \ReflectionClass($class);
+            if (!array_key_exists($key, self::$cache)) {
+                $attrs = self::reflection($class)->getAttributes($attribute);
 
-                // get the classes attributes
-                $attrs = $reflection->getAttributes($attribute);
-
-                // assign the cache value
-                self::$cache[$key] = $attrs[0]->newInstance();
+                self::$cache[$key] = $attrs ? $attrs[0]->newInstance() : null;
             }
-            // return the cached value
+
             return self::$cache[$key];
+        }
+
+        /**
+         * Determines whether a class has an attribute.
+         *
+         * @param string $class
+         * @param string $attribute
+         *
+         * @return bool
+         */
+        public static function has(string $class, string $attribute): bool
+        {
+            return self::get($class, $attribute) !== null;
+        }
+
+        /**
+         * Returns a ReflectionClass instance.
+         *
+         * @param string $class
+         *
+         * @return \ReflectionClass
+         */
+        private static function reflection(string $class): \ReflectionClass
+        {
+            return self::$reflections[$class] ??= new \ReflectionClass($class);
         }
     }
 }
