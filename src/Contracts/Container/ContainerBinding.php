9d67e15c7809a4928918c71bbd1bbb6e254eac0a
jason-napolitano <jnapolitanoit@gmail.com>
2026-07-14 07:08:58 -0700

    Update Application  - Removed the `HasOptions` trait and placed all of its code into `FrameworkFactory\Application` since no other classes would be using it  - Cleaned up the docblock for the `ContainerBinding` class

--- src/Contracts/Container/ContainerBinding.php
+++ src/Contracts/Container/ContainerBinding.php
@@ -2,20 +2,17 @@
 
 namespace FrameworkFactory\Contracts\Container {
 
-
 	final readonly class ContainerBinding
 	{
 		/**
-		 * A class that models the shape of a service being bound
-		 * into the container
+		 * A class that models the shape of a container binding
 		 *
-		 * @param mixed $factory the callable to load as the key
-		 * @param bool  $shared  is the binding a shared instance?
+		 * @param mixed $factory
+		 * @param bool  $shared
 		 */
 		public function __construct(
 			public mixed $factory,
 			public bool $shared
-		) {
-		}
+		) {}
 	}
 }
